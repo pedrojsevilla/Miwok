c1cb960ca964af70ca0e6be24534974ca503f92f
pedrojsevilla <34026709+pedrojsevilla@users.noreply.github.com>
2017-11-28 16:48:13 +0100

    Mi segundo commit

--- app/src/main/java/com/indra/pjsevilla/miwok/MainActivity.java
+++ app/src/main/java/com/indra/pjsevilla/miwok/MainActivity.java
@@ -1,7 +1,11 @@
 package com.indra.pjsevilla.miwok;
 
-import android.support.v7.app.AppCompatActivity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.TextView;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +13,56 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        final TextView numbers = findViewById(R.id.numbers);
+        numbers.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                /*
+                Toast.makeText(view.getContext(), "Open the list of numbers",
+                        Toast.LENGTH_SHORT).show();
+                        */
+                Intent intent = new Intent(MainActivity.this, NumbersActivity.class);
+                startActivity(intent);
+            }
+        });
+
+        TextView phrases = findViewById(R.id.phrases);
+        phrases.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                /*
+                Toast.makeText(view.getContext(), "Open the list of phrases",
+                        Toast.LENGTH_SHORT).show();
+                        */
+
+                Intent intent = new Intent(MainActivity.this, PhrasesActivity.class);
+                startActivity(intent);
+
+            }
+        });
+        TextView family = findViewById(R.id.family);
+        family.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                /*
+                Toast.makeText(view.getContext(), "Open the list of family members",
+                        Toast.LENGTH_SHORT).show();
+                        */
+                Intent intent = new Intent(MainActivity.this, FamilyActivity.class);
+                startActivity(intent);
+            }
+        });
+        TextView colors = findViewById(R.id.colors);
+        colors.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Toast.makeText(view.getContext(), "Open the list of colors",
+                        Toast.LENGTH_SHORT).show();
+
+                Intent intent = new Intent(MainActivity.this, ColorsActivity.class);
+                startActivity(intent);
+            }
+        });
     }
 }
